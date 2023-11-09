0cabf5917fef2e416eefcd791d55403593ad1e01
Vantix <79647031+UO277876@users.noreply.github.com>
2023-11-09 20:30:29 +0100

    mini arreglos

--- XML/xml_v2/xml/xml2perfil.py
+++ XML/xml_v2/xml/xml2perfil.py
@@ -17,14 +17,45 @@ def main():
 
     # Para escribir el nombre de archivo de los kml uso index
     index = 1
+
     for ruta in root.findall('.//{http://www.uniovi.es}ruta'):
+        distancia = 0.0
+        altura = 0
+        coordx = []
+        coordy = []
+        nombres = []
+
         # Prologo svg
         svg = '<?xml version="1.0" encoding="utf-8"?>\n'
-        svg += '<svg width="4000" height="auto" style="overflow:visible " version="1.1" xmlns="http://www.w3.org/2000/svg">\n'
+        svg += '<svg xmlns="http://www.w3.org/2000/svg" version="2.0">\n'
+        svg += '<polyline points=\n'
 
-       
+        # (distancia,altura)
+        altura = ruta.find("{http://www.uniovi.es}coordenadas/{http://www.uniovi.es}altitud").text
+        coordx.append(str(distancia))
+        coordy.append(str(altura))
+        nombres.append(ruta.find("{http://www.uniovi.es}nombre").text)
+        svg += '"' + str(distancia) + "," + str(altura) + '\n'
 
-        # Epilogo
+        for hito in ruta.findall('{http://www.uniovi.es}hitos/{http://www.uniovi.es}hito'):
+            distancia += float(hito.find("{http://www.uniovi.es}distancia").text)
+            altura = hito.find("{http://www.uniovi.es}coordenadas/{http://www.uniovi.es}altitud").text
+            coordx.append(str(distancia))
+            coordy.append(str(altura))
+            nombres.append(hito.find("{http://www.uniovi.es}hnombre").text)
+            svg += str(distancia) + "," + str(altura) + '\n'
+
+        svg += '"\n'
+        # Fin coordenadas
+        svg += 'style="fill:white;stroke:red;stroke-width:4" />\n'
+
+        # Texts
+        for i in range(len(coordx)):
+            svg += '<text x="' + coordx[i] + '" y="' + coordy[i] + '" style="writing-mode: tb; glyph-orientation-vertical: 0;">\n'
+            svg += nombres[i] + "\n"
+            svg += "</text>\n"
+
+        # Epilogo svg
         svg += "</svg>"
 
         # Creacion del archivo de salida
