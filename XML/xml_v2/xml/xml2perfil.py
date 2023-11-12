89b2cb339da81d437b663eaa74bc4f36740cccf6
Vantix <79647031+UO277876@users.noreply.github.com>
2023-11-13 00:01:31 +0100

    act

--- XML/xml_v2/xml/xml2perfil.py
+++ XML/xml_v2/xml/xml2perfil.py
@@ -1,17 +1,12 @@
 import xml.etree.ElementTree as ET
 
-def comprobacion_altura(altura: float):
-    if(altura < 2):
-        return altura * 1000
-    return altura
-
 def comprobacion_distancia(distancia: float):
-    if(distancia > 1000):
-        return distancia/1000
-    return distancia
+    # Conversión de la distancia
+    return distancia/100
 
 def main():
-    nombreXML = input('Introduzca un archivo XML = ')
+    # nombreXML = input('Introduzca un archivo XML = ')
+    nombreXML = "rutasEsquema.xml"
 
     # Leer xml
     try:
@@ -24,45 +19,65 @@ def main():
         exit()
 
     root = tree.getroot()
-
     # Para escribir el nombre de archivo de los kml uso index
     index = 1
 
     for ruta in root.findall('.//{http://www.uniovi.es}ruta'):
+        alturaMax = 0.0
         distancia = 0.0
-        altura = 0.0
+        altura = alturaMax
         coordx = []
         coordy = []
         nombres = []
 
-        # Prologo svg
-        svg = '<?xml version="1.0" encoding="utf-8"?>\n'
-        svg += '<svg xmlns="http://www.w3.org/2000/svg" version="2.0">\n'
-        svg += '<polyline points=\n'
-
-        # (distancia,altura)
-        altura = comprobacion_altura(float(ruta.find("{http://www.uniovi.es}coordenadas/{http://www.uniovi.es}altitud").text))
-        coordx.append(str(comprobacion_distancia(distancia)))
-        coordy.append(str(altura/2))
+        # Conversion: (distancia/100,alturaMax - altura)
+        alturaMax = float(ruta.find("{http://www.uniovi.es}coordenadas/{http://www.uniovi.es}altitud").text)
+        # Sumo 10 para que quede un poco mas centrado
+        coordx.append(str(distancia + 10))
+        coordy.append(alturaMax)
         nombres.append(ruta.find("{http://www.uniovi.es}nombre").text)
-        svg += '"' + str(comprobacion_distancia(distancia)) + "," + str(altura/2) + '\n'
 
         for hito in ruta.findall('{http://www.uniovi.es}hitos/{http://www.uniovi.es}hito'):
             distancia += float(hito.find("{http://www.uniovi.es}distancia").text)
-            altura = comprobacion_altura(float(hito.find("{http://www.uniovi.es}coordenadas/{http://www.uniovi.es}altitud").text))
-            coordx.append(str(comprobacion_distancia(distancia)))
-            coordy.append(str(altura/2))
+            altura = float(hito.find("{http://www.uniovi.es}coordenadas/{http://www.uniovi.es}altitud").text)
+            if(altura > alturaMax):
+               alturaMax =  altura       
+            coordx.append(str(comprobacion_distancia(distancia) + 10))
+            coordy.append(altura)
             nombres.append(hito.find("{http://www.uniovi.es}hnombre").text)
-            svg += str(comprobacion_distancia(distancia)) + "," + str(altura/2) + '\n'
 
-        svg += '"\n'
-        # Fin coordenadas
-        svg += 'style="fill:white;stroke:red;stroke-width:4" />\n'
+        # Prologo svg
+        svg = '<?xml version="1.0" encoding="utf-8"?>\n'
+        svg += '<svg xmlns="http://www.w3.org/2000/svg" version="2.0">\n'
+
+        # Conversion
+        altura = 0.0
+        for i in range(len(coordx)):
+            coordy[i] = alturaMax - coordy[i]
+            if coordy[i] > altura:
+                altura = coordy[i]
+
+        # Punto final, para cerrar. Sumo 10 para que quede un poco mas centrado
+        coordx.append(str(comprobacion_distancia(distancia) + 10))
+        coordy.append(altura)
+        nombres.append("")
+
+        # Points 
+        svg += '<polyline points=\n'
+        svg += '"'
+        for j in range(len(coordx)):
+            if j == len(coordx) - 1:
+                svg += coordx[j] + "," + str(coordy[j])
+                svg += '"\n'
+            else:
+                svg += coordx[j] + "," + str(coordy[j]) + '\n'
+        svg += 'style="fill:white;stroke:blue;stroke-width:4" />\n'
 
         # Texts
-        for i in range(len(coordx)):
-            svg += '<text x="' + coordx[i] + '" y="' + coordy[i] + '" style="writing-mode: tb; glyph-orientation-vertical: 0;">\n'
-            svg += "\t" + nombres[i] + "\n"
+        for k in range(len(coordx)):
+            # Se suma 5 a la altura para que los textos tengan un espaciado respecto a la linea
+            svg += '<text x="' + coordx[k] + '" y="' + str(altura + 5) + '" style="writing-mode: tb; glyph-orientation-horizontal: 0;">\n'
+            svg += "\t" + nombres[k] + "\n"
             svg += "</text>\n"
 
         # Epilogo svg
