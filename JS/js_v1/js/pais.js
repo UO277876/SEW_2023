81c9a001526fc5dcad1377615467f92ba9eebb3c
UO277876 <79647031+UO277876@users.noreply.github.com>
2023-11-14 12:52:21 +0100

    p1 js

---
+++ JS/js_v1/js/pais.js
@@ -0,0 +1,38 @@
+class Pais{
+    constructor(nombre, capital, poblacion){
+        this.nombre = nombre;
+        this.capital = capital;
+        this.poblacion = poblacion;
+    }
+
+    setInfo(gobierno, coordenadas, religion){
+        this.gobierno = gobierno;
+        this.coordenadas = coordenadas;
+        this.religion = religion;
+    }
+
+    getNombre(){
+        return "Nombre: " + this.nombre;
+    }
+
+    getCapital(){
+        return "Capital: " + this.capital;
+    }
+
+    getInfo(){
+        return "<ul><li> Población: " + this.poblacion + "</li>\n"
+            + "<li> Forma de gobierno: " + this.gobierno + "</li>\n"
+            + "<li> Religión mayoritaria: " + this.religion + "</li></ul>";
+    }
+
+    getCoordenadas(){
+        document.write("<p> Coordenadas: " + this.coordenadas + "</p>");
+    }
+}
+
+var pais = new Pais("El Salvador","San Salvador","6,314 millones");
+pais.setInfo("Democracia","13.794185,-88.89653,600","Católica");
+document.write("<p>" + pais.getNombre() + "</p>");
+document.write("<p>" + pais.getCapital() + "</p>");
+document.write("<p> Información secundaria:</p>" + pais.getInfo());
+pais.getCoordenadas();
