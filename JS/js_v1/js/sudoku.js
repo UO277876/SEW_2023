cf3dcceb27ad9347c25544580d0f447ae327781e
Vantix <79647031+UO277876@users.noreply.github.com>
2023-11-21 01:28:06 +0100

    casi funcionando

--- JS/js_v1/js/sudoku.js
+++ JS/js_v1/js/sudoku.js
@@ -63,10 +63,11 @@ class Sudoku {
 
                 if(this.tablero[i][j] == 0){
                     // Evento que pulsa una casilla y cambia su estado a clicked
+                    // si ya hay una pulsada elimina su estado clicked
                     cells[aux].addEventListener('click', function cellClick(){
                         var cellClicked = document.querySelector("p[data-state='clicked']");
                         if(cellClicked != null){
-                            cellClicked.dataset.state = "";
+                            cellClicked.removeAttribute("data-state");
                         }
 
                         this.dataset.state = "clicked"
@@ -86,18 +87,64 @@ class Sudoku {
     * Comprueba si el numero pulsado es valido para la casilla seleccionada
     */
     introduceNumber(number) {
-        var cellClicked = document.querySelector("p[data-state='clicked']");
+        var cells = document.querySelectorAll("p");
+        var row = 0;
+        var column = 0;
         var aux = 0;
+        var cellFound = false;
 
+        // Se saca la fila y columna del p que esta en state clicked
         for (let i=0; i < this.tablero.length; i++) {
             for (let j=0; j < this.tablero[i].length; j++) {
-                if(this.tablero[i][j]){
-                    
+                if(cells[aux].dataset.state == "clicked"){
+                    row = i;
+                    column = j;
+                    cellFound = true;
+                    break;
                 }
 
                 aux++;
             }
+
+            if(cellFound){
+                break;
+            }
         }
+
+        // Comprobaciones
+        // Comprobacion de numero en fila
+        for(let j=0; j < this.tablero[row].length; j++){
+            if(this.tablero[row][j] == number){
+                return;
+            }
+        }
+
+        // Comprobacion de numero en columna
+        for(let i=0; i < this.tablero.length; i++){
+            if(this.tablero[i][column] == number){
+                return;
+            }
+        }
+
+        // Comprobacion de numero 3x3
+        var rowAux = row%3;
+        var columnAux = column%3;
+        for(let i=rowAux; rowAux < 3; rowAux++) {
+            for(let j=columnAux; columnAux < 3; columnAux++){
+                if(this.tablero[i][j] == number){
+                    return;
+                }
+            }
+        }
+
+        // Todo correcto
+        // Asignaciones de texto y a la matriz
+        cells[aux].innerText = number;
+        this.tablero[row][column] = number;
+
+        // Estado de la celda
+        cells[aux].dataset.state = "correct";
+        cells[aux].removeEventListener('click');
     }
 
 }
@@ -115,7 +162,7 @@ document.addEventListener('keydown', function(e) {
         // Comprueba si la casilla seleccionada esta en estado clicked (por si acaso)
         if(cellClicked.dataset.state == "clicked"){
             if(e.key >= "1" && e.key <= "9"){
-                game.introduceNumber(e.key).bind(cellClicked);
+                game.introduceNumber(e.key);
             } else {
                 alert("La tecla seleccionada no es correcta")
             }
